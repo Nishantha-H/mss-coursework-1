55973e7ed04b8bd968674bd18529ff8859c54212
Malinda Weerasinghe <malinda.weerasinghe@thebetmakers.com>
2020-01-03 13:27:03 +0530

    Fiz prescription save issue

--- app/PrescriptionItem.php
+++ app/PrescriptionItem.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class PrescriptionItem extends Model
 {
     protected $table = 'prescription_items';
+    protected $guarded = [];
+    public $timestamps = false;
 
     public function prescription()
     {
